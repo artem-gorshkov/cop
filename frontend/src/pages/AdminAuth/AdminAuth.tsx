eedc1383454787377eea17447bc79f5da0f370ca
Ivan Cheprasov <icheprasov@emphasoft.com>
2023-07-25 22:36:33 +0300

    feat: Changed the styles of the page title. Added navigation example.

--- frontend/src/pages/AdminAuth/AdminAuth.tsx
+++ frontend/src/pages/AdminAuth/AdminAuth.tsx
@@ -4,16 +4,16 @@ import { Button, Layout, Typography } from 'antd';
 import { Link } from 'react-router-dom';
 import { ROUTES } from 'constants/routes';
 
-export default function TestList() {
+export default function AdminAuth() {
   return (
     <Layout hasSider className="fullHeight">
       <Layout.Content>
-        <Typography.Title>Тесты</Typography.Title>
+        <Typography.Title>Вход для преподавателя</Typography.Title>
       </Layout.Content>
       <Layout.Sider width={500} className="fullHeight">
         <Button>
-          <Link to={ROUTES.ADMIN_AUTH}>
-            <span>Вход преподавателя</span>
+          <Link to={ROUTES.TEST_LIST}>
+            <span>Назад</span>
           </Link>
         </Button>
       </Layout.Sider>
