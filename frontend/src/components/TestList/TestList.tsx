0afcce0ce0a7e19d154a805bae41a7abe7d7def8
Ivan Cheprasov <icheprasov@emphasoft.com>
2023-07-29 05:07:55 +0300

    feat: Added icons to the test list.

--- frontend/src/components/TestList/TestList.tsx
+++ frontend/src/components/TestList/TestList.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Row } from "antd";
+import { Button, Row } from "antd";
 import { Test } from "types/test";
 import { ROUTES } from "constants/routes";
 import { Link } from "react-router-dom";
@@ -8,6 +8,8 @@ import styles from './TestList.scss';
 import { useQuery } from "@tanstack/react-query";
 import Api from "services/api";
 import Loader from "components/Loader";
+import { CloseOutlined, SettingOutlined } from "@ant-design/icons";
+import cx from "classnames";
 
 interface TestListProps {
   isEntitled?: boolean;
@@ -26,10 +28,20 @@ export default function TestList({ isEntitled = false }: TestListProps) {
         <Loader />
       ) : (
         data?.map((test) => (
-          <Row className={styles.testListItem} key={test.id}>
-            <Link to={`${ROUTES.AUTH}?testId=${test.id}`}>
+          <Row className={cx(styles.testListItem, isEntitled && styles.isEntitled)} key={test.id}>
+            <Link className={styles.testName} to={`${ROUTES.AUTH}?testId=${test.id}`}>
               <span>{test.name}</span>
             </Link>
+            {isEntitled && (
+             <>
+               <Button>
+                 <Link to={ROUTES.ADMIN_AUTH}>
+                   <SettingOutlined />
+                 </Link>
+               </Button>
+               <Button icon={<CloseOutlined />} />
+             </>
+            )}
           </Row>
         ))
       )}
