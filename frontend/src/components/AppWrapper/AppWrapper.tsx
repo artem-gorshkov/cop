72921294fa58b8ef881e117a585ab2c67d36e1d5
Ivan Cheprasov <icheprasov@emphasoft.com>
2023-09-19 15:57:52 +0300

    feat: added the header logo.

--- frontend/src/components/AppWrapper/AppWrapper.tsx
+++ frontend/src/components/AppWrapper/AppWrapper.tsx
@@ -1,4 +1,4 @@
-import { Layout } from 'antd';
+import { Layout, Image } from 'antd';
 import { Suspense } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import Loader from 'components/Loader';
@@ -6,6 +6,7 @@ import styles from './AppWrapper.scss';
 import { useAppContext } from "contexts/AppContext";
 import { ROUTES } from "constants/routes";
 import cx from "classnames";
+import HeaderLogo from "images/header-logo.png";
 
 export default function AppWrapper() {
   const { headerText, isEntitled } = useAppContext();
@@ -16,8 +17,13 @@ export default function AppWrapper() {
   return (
     <Suspense fallback={<Loader isCentered />}>
       <Layout>
-        <Layout.Header className={cx(styles.header, isOnAdminPage && styles.adminHeader)} title={headerText}>
-          {headerText}
+        <Layout.Header className={cx(styles.header, isOnAdminPage && styles.adminHeader)}>
+          <div className={styles.logoWrapper}>
+            <Image src={HeaderLogo} width={100} preview={false} />
+            <span title={headerText}>
+              {headerText}
+            </span>
+          </div>
         </Layout.Header>
         <Layout.Content className={styles.content}>
           <Outlet />
